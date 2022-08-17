cd6781c6eb28069b36cc58e700b453454aa34277
Isita Polamarasetti <78599202+isita-431@users.noreply.github.com>
2022-08-17 20:13:52 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,10 +1,13 @@
 import streamlit as st 
 import pandas as pd 
+from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 import sys
 import os
 import streamlit.components.v1 as stc
+import matplotlib.pyplot as plt
 import sqlite3
+import csv
 conn = sqlite3.connect('data.db',check_same_thread=False)
 cur = conn.cursor()
 st.set_page_config(page_title='Quiz',layout='wide')
@@ -20,7 +23,20 @@ def addData(name,a,b,c,d,e):
 	conn.commit()
 	conn.close()
 	st.success('Successfully submitted')
+    
+# def create_csv():
+#     with open('output.csv', 'a+',  newline = '') as file_output:
+#         headers = ["name","q1","q2","q3","q4","q5"]
+#         writer = csv.DictWriter(file_output, delimiter=',', lineterminator='\n',fieldnames=headers)
+#         writer.writeheader()
 
+def update_csv(name,a,b,c,d,e):
+    with open('output.csv', 'a+',  newline = '') as file_output:
+        # csv_writer.writerows(rows) 
+        csv_writer = csv.writer(file_output)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow([name,a[0],b[0],c[0],d[0],e[0]])
+        
 with st.form(key='quiz form'):
     name = st.text_input(label='Enter your name : ')
     st.markdown('You are given a  csv file  “student_records.csv” , dataframe “student_records” , “Student name” and “marks” are two columns in the dataframe.')
@@ -53,5 +69,43 @@ with st.form(key='quiz form'):
     q5 = st.radio("5.Select the right way to select “Student name” from student_records dataframe." ,l5)
     e = ['right' if q5==l5[2] else 'wrong']
     submission = st.form_submit_button(label='Submit')
+    # create_csv()
     if submission == True:
         addData(name,a,b,c,d,e)
+        update_csv(name,a,b,c,d,e)
+
+# def display(col_name):
+#     cur.execute("""select """+col_name+' from clg_form;')
+b = st.button('click here for result summary : ')
+if b:
+    df = pd.read_csv('output.csv')
+    st.write('First Question :')
+    arr = df['q1']
+    fig, ax = plt.subplots()
+    ax.bar(arr.value_counts().index,arr.value_counts())
+    plt.figure(figsize=(6,8))
+    st.pyplot(fig)  
+    st.write('Second Question :')
+    arr2 = df['q2']
+    fig, ax = plt.subplots()
+    ax.bar(arr2.value_counts().index,arr2.value_counts())
+    plt.figure(figsize=(6,8))
+    st.pyplot(fig)  
+    st.write('Third Question :')
+    arr3 = df['q3']
+    fig, ax = plt.subplots()
+    ax.bar(arr3.value_counts().index,arr3.value_counts())
+    plt.figure(figsize=(6,8))
+    st.pyplot(fig)  
+    st.write('Fourth Question :')
+    arr4 = df['q4']
+    fig, ax = plt.subplots()
+    ax.bar(arr4.value_counts().index,arr4.value_counts())
+    plt.figure(figsize=(6,8))
+    st.pyplot(fig)  
+    st.write('Fifth Question :')
+    arr5 = df['q5']
+    fig, ax = plt.subplots()
+    ax.bar(arr5.value_counts().index,arr5.value_counts())
+    plt.figure(figsize=(2,2))
+    st.pyplot(fig)  
